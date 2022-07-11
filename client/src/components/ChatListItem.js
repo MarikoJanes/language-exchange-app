2bfd0cf8759678741017f44c8e288d3bed9f08c3
Mariko Janes <Marikojanes@gmail.com>
2022-07-11 10:34:05 -0600

    added chat-partner card

--- client/src/components/ChatListItem.js
+++ client/src/components/ChatListItem.js
@@ -7,12 +7,6 @@ function ChatListItem({ conversation, userData }) {
   const [partner, setPartner] = useState(null);
   const history = useHistory();
 
-  // if(userData.id === conversation.user_id) {
-  //   return setPartner(conversation.partner_id);
-  // } else if (userData.id !== conversation.user_id) {
-  //   return setPartner(conversation.user_id);
-  // };
-
   useEffect(() => {
     let fetchId;
     if(userData.id == conversation.user_id) {
@@ -22,20 +16,38 @@ function ChatListItem({ conversation, userData }) {
     };
     fetch(`/users/${fetchId}`)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => setPartner(data));
   }, []);
 
-    function handlePageJump(e) {
-        const pageId = e.target.parentElement.id;
-        history.push(`/chatrooms/${pageId}`);
+    function handlePageJump() {
+        history.push(`/chatrooms/${conversation.id}`);
     }
 
-console.log(conversation);
+console.log(partner);
+if(partner === null) return <h2>Loading...</h2>
   return (
-    <div id={conversation.id}>
-      {conversation.id}
+    <>
+    {Object.keys(partner).length > 0 ?
+      <div id={partner.id}>
+      <h3>{partner.name}</h3>
+      <ul>
+        learning: 
+          {partner.language_to_learns.length > 0 ?
+            partner.language_to_learns.map(lang => {
+              return <li key={lang.id}>{lang.name}</li>
+            }) : null}
+      </ul>
+      <ul>
+        teaching: 
+          {partner.language_to_teaches.length > 0 ?
+            partner.language_to_teaches.map(lang => {
+              return <li key={lang.id}>{lang.name}</li>
+            }) : null}
+      </ul>
       <button onClick={handlePageJump} >Chat</button>
-    </div>
+    </div> : null
+    }
+    </>
   )
 }
 
